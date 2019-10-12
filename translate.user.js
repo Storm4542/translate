15e7972e8ead9ead64cf781b3aaca2459d58e6c2
Storm4542 <417002107@qq.com>
2019-10-12 15:09:10 +0800

    change version

--- translate.user.js
+++ translate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         划词翻译
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      1.1.2
 // @description  try to take over the world!
 // @author       You
 // @grant        none
